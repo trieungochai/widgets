818c2e2aca3c829692a26f272f088e8c162fd67c
Cai <trieungochai.dev@gmail.com>
2021-07-03 21:33:47 +0700

    Async Code in useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get("cowBoyBebop");
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
