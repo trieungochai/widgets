683b06b3f0dfe14c6cc6ca143d9dee2f8eaafd62
Cai <trieungochai.dev@gmail.com>
2021-07-04 16:49:59 +0700

    Searching on Initial Render

--- src/components/Search.js
+++ src/components/Search.js
@@ -22,16 +22,20 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    const timeoutId = setTimeout(() => {
-      // err: The \"srsearch\" parameter must be set.
-      if (term) {
-        search();
-      }
-    }, 1000);
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        // err: The \"srsearch\" parameter must be set.
+        if (term) {
+          search();
+        }
+      }, 1000);
 
-    return () => {
-      clearTimeout(timeoutId);
-    };
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
