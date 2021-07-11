c989752ea98a95a83bda85abd60bffb99b43d83e
Cai <trieungochai.dev@gmail.com>
2021-07-11 14:30:19 +0700

    Selection State

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Dropdown from "./components/Dropdown";
 
@@ -36,10 +36,16 @@ const options = [
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
       {/* <Accordion items={items} />; */}
-      <Dropdown options={options} />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
