154d5771d68195d05b32179cee284c6b1c27a32f
Cai <trieungochai.dev@gmail.com>
2021-07-21 02:24:28 +0700

    Building a Reuseable Route Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Translate from "./components/Translate";
 import Dropdown from "./components/Dropdown";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Route from "./components/Route";
 
 const items = [
   {
@@ -35,34 +36,52 @@ const options = [
   },
 ];
 
-const showAccordion = () => {
-  if (window.location.pathname === "/") {
-    return <Accordion items={items} />;
-  }
-};
+// const showAccordion = () => {
+//   if (window.location.pathname === "/") {
+//     return <Accordion items={items} />;
+//   }
+// };
 
-const showList = () => {
-  if (window.location.pathname === "/list") {
-    return <Search />;
-  }
-};
+// const showList = () => {
+//   if (window.location.pathname === "/list") {
+//     return <Search />;
+//   }
+// };
 
-const showDropdown = () => {
-  if (window.location.pathname === "/dropdown") {
-    return <Dropdown />;
-  }
-};
+// const showDropdown = () => {
+//   if (window.location.pathname === "/dropdown") {
+//     return <Dropdown />;
+//   }
+// };
 
-const showTranslate = () => {
-  if (window.location.pathname === "/translate") {
-    return <Translate />;
-  }
-};
+// const showTranslate = () => {
+//   if (window.location.pathname === "/translate") {
+//     return <Translate />;
+//   }
+// };
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      {showAccordion()} {showList()} {showDropdown()} {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
