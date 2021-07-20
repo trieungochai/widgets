b2854582a9aea5aea5000f3ddc415c46eca843da
Cai <trieungochai.dev@gmail.com>
2021-07-21 02:04:45 +0700

    Basic Component Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 import Translate from "./components/Translate";
-// import Dropdown from "./components/Dropdown";
-// import Accordion from "./components/Accordion";
-// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -35,10 +35,34 @@ const options = [
   },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
 const App = () => {
   return (
     <div>
-      <Translate />
+      {showAccordion()} {showList()} {showDropdown()} {showTranslate()}
     </div>
   );
 };
