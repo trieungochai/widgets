72043a17bf11fb473d0f34875074a2fc5ccf49b7
Cai <trieungochai.dev@gmail.com>
2021-07-21 23:05:42 +0700

    Changing the URL (but don't do a full page refresh)

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Link = ({ className, href, children }) => {
+  const onClick = (event) => {
+    event.preventDefault();
+    window.history.pushState({}, "", href);
+  };
+
   return (
-    <a className={className} href={href}>
+    <a onClick={onClick} className={className} href={href}>
       {children}
     </a>
   );
